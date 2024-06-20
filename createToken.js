b9972fcbd5b7d580afbf6bc2e6bcffcfcde283e4
Denis Dinkov <denisdinkov7@gmail.com>
2024-06-20 14:35:46 +0300

    Create Token

---
+++ createToken.js
@@ -0,0 +1,48 @@
+console.clear();
+
+const {
+  Client,
+  PrivateKey,
+  Hbar,
+  TokenCreateTransaction,
+  TokenType,
+} = require("@hashgraph/sdk");
+require("dotenv").config();
+
+async function createToken() {
+  const accId = process.env.MY_ACCOUNT_ID;
+  const privateKey = process.env.MY_PRIVATE_KEY;
+  const supplyKey = PrivateKey.generate();
+
+  if (!accId || !privateKey) {
+    throw new Error(
+      "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present"
+    );
+  }
+  const client = Client.forTestnet()
+    .setOperator(accId, PrivateKey.fromStringECDSA(privateKey))
+    .setDefaultMaxTransactionFee(new Hbar(100))
+    .setDefaultMaxQueryPayment(new Hbar(50));
+
+  let tokenCreateTx = new TokenCreateTransaction()
+    .setTokenName("Bitcoin")
+    .setTokenSymbol("BTC")
+    .setTokenType(TokenType.FungibleCommon)
+    .setDecimals(8)
+    .setInitialSupply(210000)
+    .setTreasuryAccountId(accId)
+    .setSupplyKey(supplyKey)
+    .freezeWith(client);
+
+  const tokenCreateSign = await tokenCreateTx.sign(
+    PrivateKey.fromStringECDSA(privateKey)
+  );
+  const tokenCreateSubmit = await tokenCreateSign.execute(client);
+  const tokenCreateRx = await tokenCreateSubmit.getReceipt(client);
+  const tokenId = tokenCreateRx.tokenId;
+
+  console.log("Created token with ID:" + tokenId);
+  return;
+}
+
+createToken();
